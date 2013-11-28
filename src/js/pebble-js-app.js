449186f963cb2c8b51fb1d9a3c0bac299f46b3cd
mikegrb <mgreb@linode.com>
2013-11-28 13:03:12 -0400

    pebble-js support for changing setpoint

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -6,10 +6,30 @@ Pebble.addEventListener("ready",
 
 Pebble.addEventListener("appmessage",
   function (e) {
-    console.log("message");
+    console.log("Received message: " + e.payload);
     fetchData();
   }
 );
+
+function changeSetpoint(setpoint) {
+  var response;
+  var req = new XMLHttpRequest();
+  req.open('GET', "http://thegrebs.com/~michael/.thermostat.pl?set=" + setpoint + "&token=" + Pebble.getAccountToken(), true);
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        console.log(req.responseText);
+        response = JSON.parse(req.responseText);
+        if (response.temp) {
+          Pebble.sendAppMessage(response);
+        }
+      } else {
+        console.log("Request returned error code " + req.status.toString());
+      }
+    }
+  };
+  req.send(); 
+}
 
 function fetchData() {
   var response;
