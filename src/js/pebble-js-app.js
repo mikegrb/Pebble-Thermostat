a4082047c6e27f579d63ce6d862f677d3845bfa8
mikegrb <mgreb@linode.com>
2013-12-02 17:50:47 -0500

    cleanup js

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,6 +1,9 @@
+var base_url;
+
 Pebble.addEventListener("ready",
   function (e) {
       console.log("ready");
+      base_url = 'http://thegrebs.com/~michael/.thermostat.pl?token=' + Pebble.getAccountToken();
   }
 );
 
@@ -8,7 +11,7 @@ Pebble.addEventListener("appmessage",
   function (e) {
     if(e.payload.set) {
       console.log("Received set message: " + e.payload.set);
-      changeSetpoint(e.payload.set);
+      fetchData(e.payload.set);
     }
     else {
       console.log("Received fetch message");
@@ -17,30 +20,12 @@ Pebble.addEventListener("appmessage",
   }
 );
 
-function changeSetpoint(setpoint) {
+function fetchData(setpoint) {
   var response;
-  var req = new XMLHttpRequest();
-  req.open('GET', "http://thegrebs.com/~michael/.thermostat.pl?set=" + setpoint + "&token=" + Pebble.getAccountToken(), true);
-  req.onload = function(e) {
-    if (req.readyState == 4) {
-      if(req.status == 200) {
-        console.log(req.responseText);
-        response = JSON.parse(req.responseText);
-        if (response.temp) {
-          Pebble.sendAppMessage(response);
-        }
-      } else {
-        console.log("Request returned error code " + req.status.toString());
-      }
-    }
-  };
-  req.send(); 
-}
+  var url = setpoint ? base_url : base_url + "&set=" + setpoint;
 
-function fetchData() {
-  var response;
   var req = new XMLHttpRequest();
-  req.open('GET', "http://thegrebs.com/~michael/.thermostat.pl?token=" + Pebble.getAccountToken(), true);
+  req.open('GET', url, true);
   req.onload = function(e) {
     if (req.readyState == 4) {
       if(req.status == 200) {
